e5d8107f801265e26ca6b89c3448b8d58723474c
William Monte de Moura <williamonte@gmail.com>
2023-03-25 15:30:08 -0300

    Exercício 01

---
+++ Exercicio_S07/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Header } from './components/Header'
+
+export const App = () => {
+
+  return(
+    <div>
+      {/* COMPONENTES */}
+      <Header titulo="Negociações"/>
+    </div>
+  );
+};
+
+export default App
---
+++ Exercicio_S07/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+export const Header = (props) => {
+    return(
+        <h1 className="text-center py-5 bg-primary text-light">{props.titulo}</h1>
+    );
+};
+
+Header.PropTypes = {
+    titulo: PropTypes.string.isRequired,
+};
